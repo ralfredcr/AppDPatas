aee300bf3b2fa2c23239ee8abbbd31c691d2360b
Josue Durand Pebe <josuedurandpebe@MacBook-Pro-de-Josue.local>
2020-09-13 11:49:54 -0500

    a

--- app/src/main/java/com/example/dpatas/Home.java
+++ app/src/main/java/com/example/dpatas/Home.java
@@ -1,15 +1,16 @@
 package com.example.dpatas;
 
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.WindowManager;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 import com.example.dpatas.fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Home extends AppCompatActivity {
 
--- app/src/main/java/com/example/dpatas/MainActivity.java
+++ app/src/main/java/com/example/dpatas/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.dpatas;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
